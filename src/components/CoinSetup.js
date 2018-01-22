0d9b58dc850d5dd9e01df9a610ea8014c6b3da18
Marius Skaar LudVigsen <marius.ludvigsen@gmail.com>
2018-01-22 23:19:18 +0100

    WIP

--- src/components/CoinSetup.js
+++ src/components/CoinSetup.js
@@ -5,65 +5,190 @@ import TextField from 'material-ui/TextField';
 import Slider from 'material-ui/Slider';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import Web3 from 'web3';
+import contract from 'truffle-contract';
+import BookPub from '../contracts/BookPub';
 
+const web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+console.log(web3.eth.accounts);
+console.log(web3);
+console.log(BookPub);
+
+const network = Object.keys(BookPub.networks)[0];
+const address = BookPub.networks[network].address;
+console.log(address);
+const bookPub = contract(BookPub);
+bookPub.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+console.log(bookPub);
+
+let bp;
+bookPub.deployed().then(instance => {
+  console.log(instance);
+  bp = instance;
+});
 
 class CoinSetup extends Component {
-    state = {
-      value: 1,
-      secondSlider: 50,
-    };
+  state = {
+    readershipStake: 100,
+  };
 
-    handleChange = (event, index, value) => this.setState({value});
-
-    handleSecondSlider = (event, value) => {
-      this.setState({secondSlider: value});
+  handleChange = (fieldName, event) => {
+    const state = {
+      ...this.state,
     };
-    
-    render() {
-        return (
-            <div className="coin-setup">
-                <h2>Coin Setup</h2>
-                <div>
-                    <div className="input-text">
-                      <TextField hintText="Book Name"/><br />
-                    </div>
-                    <div className="public-equity"> 
-                        <p>
-                            <span>{'Public Equity Reserve '}</span>
-                            <span className="equity-percentage">{this.state.secondSlider}{' %'}</span>
-                        </p>    
-                        <Slider
-                            min={0}
-                            max={100}
-                            step={1}
-                            value={this.state.secondSlider}
-                            onChange={this.handleSecondSlider}
-                        />
-                    </div>
-                    <div className="gov">
-                        <p>Governance</p>
-                        <SelectField
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                        >
-                            <MenuItem value={'Boardroom Integration'} primaryText="Board Room Integration" />
-                            <MenuItem value={'Traditional Equity Based'} primaryText="Traditional Equity Based" />
-                        </SelectField>
-                        <br />
-                    </div>
-                    <div>
-                      <TextField hintText="Coin Name (i.e. Ether)"/><br />
-                    </div>
-                    <div>
-                      <TextField hintText="Coin Acronym (i.e. ETH)"/><br />
-                    </div>
-                </div>
-                <Link to="/dashboard">
-                  <button>Create</button>
-                </Link>
-            </div>
-        )
-    }
+    state[fieldName] = event.target.value;
+    this.setState(state);
+  };
+
+  handleReadershipStake = (event, value) => {
+    this.setState({ readershipStake: value });
+  };
+
+  create = () => {
+    const {
+      readershipStake,
+      goal,
+      toBeShipped,
+      userCount,
+      eligibleCount,
+      initialAmount,
+      coinName,
+      symbol,
+      decimalUnits,
+    } = this.state;
+    console.log(
+      readershipStake,
+      goal,
+      toBeShipped,
+      userCount,
+      eligibleCount,
+      initialAmount,
+      coinName,
+      decimalUnits,
+      symbol,
+
+      { from: web3.eth.accounts[0] },
+    );
+    bp.publishBook(
+      readershipStake,
+      goal,
+      toBeShipped,
+      userCount,
+      eligibleCount,
+      initialAmount,
+      coinName,
+      decimalUnits,
+      symbol,
+      { from: web3.eth.accounts[0], gas: 9000000 },
+    );
+  };
+
+  render() {
+    return (
+      <div className="coin-setup">
+        <h2>Coin Setup</h2>
+        <div>
+          <div className="input-text">
+            <TextField
+              hintText="Book Name"
+              onChange={this.handleChange.bind(this, 'name')}
+            />
+            <br />
+          </div>
+          <div className="public-equity">
+            <p>
+              <span>{'Public Equity Reserve '}</span>
+              <span className="equity-percentage">
+                {this.state.secondSlider}
+                {' %'}
+              </span>
+            </p>
+            <Slider
+              min={0}
+              max={100}
+              step={1}
+              value={this.state.readershipStake}
+              onChange={this.handleReadershipStake}
+            />
+          </div>
+          <div className="gov">
+            <p>Governance</p>
+            <SelectField value={this.state.value} onChange={this.handleChange}>
+              <MenuItem
+                value={'Boardroom Integration'}
+                primaryText="Board Room Integration"
+              />
+              <MenuItem
+                value={'Traditional Equity Based'}
+                primaryText="Traditional Equity Based"
+              />
+            </SelectField>
+            <br />
+          </div>
+          <div>
+            <TextField
+              hintText="Coin Name (i.e. Ether)"
+              onChange={this.handleChange.bind(this, 'coinName')}
+            />
+            <br />
+          </div>
+          <div>
+            <TextField
+              hintText="Coin Acronym (i.e. ETH)"
+              onChange={this.handleChange.bind(this, 'symbol')}
+            />
+            <br />
+          </div>
+          <div>
+            <TextField
+              hintText="Goal"
+              onChange={this.handleChange.bind(this, 'goal')}
+            />
+            <br />
+          </div>
+          <div>
+            <TextField
+              hintText="To be shipped"
+              onChange={this.handleChange.bind(this, 'toBeShipped')}
+            />
+            <br />
+          </div>
+          <div>
+            <TextField
+              hintText="User count"
+              onChange={this.handleChange.bind(this, 'userCount')}
+            />
+            <br />
+          </div>
+          <div>
+            <TextField
+              hintText="Eligible count"
+              onChange={this.handleChange.bind(this, 'eligibleCount')}
+            />
+            <br />
+          </div>
+          <div>
+            <TextField
+              hintText="Initial amount"
+              onChange={this.handleChange.bind(this, 'initialAmount')}
+            />
+            <br />
+          </div>
+          <div>
+            <TextField
+              hintText="Decimal units"
+              onChange={this.handleChange.bind(this, 'decimalUnits')}
+            />
+            <br />
+          </div>
+        </div>
+        <Link to="/dashboard">
+          <button onClick={this.create}>Create</button>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default CoinSetup;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,83 +1,129 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Web3 from 'web3';
+import contract from 'truffle-contract';
+import BookPub from '../contracts/BookPub';
 
+const web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+const network = Object.keys(BookPub.networks)[0];
+const address = BookPub.networks[network].address;
+const bookPub = contract(BookPub);
+bookPub.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+let bp;
 
 class Dashboard extends Component {
+  state = {};
+  constructor(props) {
+    super(props);
+    bookPub.deployed().then(instance => {
+      console.log(instance);
+      bp = instance;
+      instance.books(0).then(book => {
+        this.setState({ book });
+        console.log(book);
+      });
+    });
+  }
   render() {
-      return (
-          <div>
-              <div className="top-bar">
-                  <p>Current Market Price XXX = $X | 7d avg inflation = -5%</p>
-              </div>
-              <div className="public-coin">
-                  <h2>Public Coin Dashboard</h2>
-                  <div className="box-1">
-                    <div className="details">
-                        <div>
-                            <span className="the-name">Current Goal</span>
-                            <span className="the-value">(none)</span>
-                        </div>
-                        <div>
-                            <span className="the-name">Funds Raised</span>
-                            <span className="the-value">(none)</span>
-                        </div>
-                        <div>
-                            <span className="the-name">Public Coins</span>
-                            <span className="the-value">150</span>
-                        </div>
-                        <div>
-                            <span className="the-name">Operating Capital</span>
-                            <span className="the-value">150</span>
-                        </div>
-                    </div>
-                    <div className="in-buttons">
-                        <button>Funding Goal</button>
-                        <button>Users To Admit</button>
-                        <button>Sale Start</button>
-                        <button>Sale End</button>
-                        <Link to="/user">
-                          <button className="last-one">Start Funding Round</button>
-                        </Link>
-                    </div>
-                  </div>
-                  <div className="out-buttons">
-                      <Link to="/user">
-                        <button>View Queue</button>
-                      </Link>
-                      <button>Configure Regulations</button>
-                      <button>Buy Back Coins</button>  
-                  </div> 
-              </div>
-              <div className="equity-ratios">
-                  <h2>Equity Ratios</h2>
-                  <div className="box-2">
-                    <div className="equity-details">
-                        <div>
-                            <span className="the-name">Founder</span>
-                            <span className="the-value">20%</span>
-                        </div>
-                        <div>
-                            <span className="the-name">Co-Founder</span>
-                            <span className="the-value">20%</span>
-                        </div>
-                        <div>
-                            <span className="the-name">Public Coins</span>
-                            <span className="the-value">15%</span>
-                        </div>
-                        <div>
-                            <span className="the-name">To Be Allocated</span>
-                            <span className="the-value">45%</span>
-                        </div>
-                    </div>
-                    <div className="in-button">
-                        <button>Add a Partner</button>
-                        <button>Send Dividend</button>
-                        <button>Allocate Coins</button>
-                    </div>
-                  </div>
-              </div>
+    const { book } = this.state;
+    return (
+      <div>
+        {book && (
+          <div style={{ marginTop: 100 }}>
+            <h3>First book</h3>
+            <p>
+              <span>ID: </span>
+              {book[0].toString()}
+            </p>
+            <p>
+              <span>Published address: </span>
+              {book[1]}
+            </p>
+            <p>
+              <span>Owner: </span>
+              {book[2]}
+            </p>
+            <p>
+              <span>Readership stake: </span>
+              {book[3].toString()}
+            </p>
           </div>
-      )
+        )}
+        <div className="top-bar">
+          <p>Current Market Price XXX = $X | 7d avg inflation = -5%</p>
+        </div>
+        <div className="public-coin">
+          <h2>Public Coin Dashboard</h2>
+          <div className="box-1">
+            <div className="details">
+              <div>
+                <span className="the-name">Current Goal</span>
+                <span className="the-value">(none)</span>
+              </div>
+              <div>
+                <span className="the-name">Funds Raised</span>
+                <span className="the-value">(none)</span>
+              </div>
+              <div>
+                <span className="the-name">Public Coins</span>
+                <span className="the-value">150</span>
+              </div>
+              <div>
+                <span className="the-name">Operating Capital</span>
+                <span className="the-value">150</span>
+              </div>
+            </div>
+            <div className="in-buttons">
+              <button>Funding Goal</button>
+              <button>Users To Admit</button>
+              <button>Sale Start</button>
+              <button>Sale End</button>
+              <Link to="/user">
+                <button className="last-one">Start Funding Round</button>
+              </Link>
+            </div>
+          </div>
+          <div className="out-buttons">
+            <Link to="/user">
+              <button>View Queue</button>
+            </Link>
+            <button>Configure Regulations</button>
+            <button>Buy Back Coins</button>
+          </div>
+        </div>
+        <div className="equity-ratios">
+          <h2>Equity Ratios</h2>
+          <div className="box-2">
+            <div className="equity-details">
+              <div>
+                <span className="the-name">Founder</span>
+                <span className="the-value">20%</span>
+              </div>
+              <div>
+                <span className="the-name">Co-Founder</span>
+                <span className="the-value">20%</span>
+              </div>
+              <div>
+                <span className="the-name">Public Coins</span>
+                <span className="the-value">15%</span>
+              </div>
+              <div>
+                <span className="the-name">To Be Allocated</span>
+                <span className="the-value">45%</span>
+              </div>
+            </div>
+            <div className="in-button">
+              <button>Add a Partner</button>
+              <button>Send Dividend</button>
+              <button>Allocate Coins</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
